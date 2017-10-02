4d0af329fddbbc59ea3fb9a610220b7c18001724
aliccce <aliceg@outlook.com>
2017-10-02 19:35:00 +0200

    Threshold unit, not done

--- threshold/threshold_unit.scala
+++ threshold/threshold_unit.scala
@@ -1,2 +1,26 @@
 
 import chisel._
+
+class Compare_input extends Module {
+    val a = Bits(Input, width=8)    // Threshold input
+    val b = Bits(Input, width=8)    // data input
+}
+
+class Compare extends Module{
+    val io = new Bundle {
+        val in      = Decoupled(new Compare_input()).flip()
+        val out     = Bool(OUTPUT)
+    }
+
+    val thresh = Reg(Uint, width=8)
+    val data = Reg(Uint, width=8)
+    val p = Reg(init=false)
+    io.in.ready := !p
+
+    when( io.in.valid && !p ) {
+        thresh := io.in.bits.a
+        data := io.in.bits.b
+        p := Bool(true)
+
+    }
+}
