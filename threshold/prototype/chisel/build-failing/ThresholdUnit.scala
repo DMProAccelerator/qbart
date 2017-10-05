403f20a8eba3868182546b2a9e5c21586ce00728
aliccce <aliceg@outlook.com>
2017-10-05 19:40:54 +0200

    Compare unit with test

--- threshold/prototype/chisel/build-failing/ThresholdUnit.scala
+++ threshold/prototype/chisel/build-failing/ThresholdUnit.scala
@@ -1,27 +1,48 @@
-package Prototypes
+package TutorialSolutions
 
-import chisel._
+import Chisel._
 
-class CompareUnit extends Module {
-    val a = Bits(Input, width=8)    // Threshold input
-    val b = Bits(Input, width=8)    // data input
+class CompareUnitInput extends Bundle {
+    val a = Bits(width = 8)    // Threshold input
+    val b = Bits(width = 8)    // data input
 }
 
-class Compare extends Module {
+class CompareUnit extends Module {
     val io = new Bundle {
-        val in = Decoupled(new Compare_input()).flip()
-        val out = Bool(OUTPUT)
+        val in = Decoupled( new CompareUnitInput() ).flip()
+        val out = UInt(OUTPUT, 1)
     }
 
-    val thresh = Reg(Uint, width=8)
-    val data = Reg(Uint, width=8)
-    val p = Reg(init=false)
+    val thresh = Reg(UInt(width = 8))
+    val data = Reg(UInt(width = 8))
+    val p = Bool(false)
     io.in.ready := !p
+    io.out := Bool(false)
 
     when( io.in.valid && !p ) {
         thresh := io.in.bits.a
         data := io.in.bits.b
         p := Bool(true)
 
+        io.out := (data >= thresh)
+        p := Bool(false)
+    }
+}
+
+
+class CompareUnitTests(c: CompareUnit) extends Tester(c) {
+    val inputs = List( (4, 3), (4, 4), (4, 5) )    // (data, threshold)
+    val outputs = List( 0, 1, 1 )
+
+    for (en <- 0 to 1) {
+        for (i <- 0 until 3 ) {
+            poke(c.io.in.bits.a, inputs(i)._1)
+            poke(c.io.in.bits.b, inputs(i)._2)
+            poke(c.io.in.valid, en)
+
+            step(1)
+
+            expect(c.io.out, outputs(i) & en)
+        }
     }
 }
