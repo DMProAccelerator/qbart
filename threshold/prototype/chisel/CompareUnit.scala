035cae39789d28174fde09a7a3e167d6e13fbee9
aliccce <aliceg@outlook.com>
2017-10-08 18:52:17 +0200

    Compare unit is simplified

--- threshold/prototype/chisel/CompareUnit.scala
+++ threshold/prototype/chisel/CompareUnit.scala
@@ -2,45 +2,32 @@ package Prototypes
 
 import Chisel._
 
-class CompareUnitInput extends Bundle {
-    val a = Bits(width = 8)    // Threshold input
-    val b = Bits(width = 8)    // data input
-}
-
 class CompareUnit extends Module {
     val io = new Bundle {
-        val in = Decoupled( new CompareUnitInput() ).flip()
+        val data = Bits(INPUT, width = 8)
+        val thresh = Bits(INPUT, width = 8)
+        val en = Bool(INPUT)
         val out = UInt(OUTPUT, 1)
     }
 
-    val thresh = Reg(UInt(width = 8))
-    val data = Reg(UInt(width = 8))
-    val p = Bool(false)
-    io.in.ready := !p
     io.out := Bool(false)
 
-    when( io.in.valid && !p ) {
-        thresh := io.in.bits.a
-        data := io.in.bits.b
-        p := Bool(true)
-
-        io.out := (data >= thresh)
-        p := Bool(false)
+    when( io.en ) {
+        io.out := (UInt(io.data) >= UInt(io.thresh))
     }
 }
 
 
 class CompareUnitTests(c: CompareUnit) extends Tester(c) {
-    val inputs = List( (4, 3), (4, 4), (4, 5) )    // (data, threshold)
+    val inputs = List( 3, 4, 5 )
+    val thresh = 4
     val outputs = List( 0, 1, 1 )
 
     for (en <- 0 to 1) {
         for (i <- 0 until 3 ) {
-            poke(c.io.in.bits.a, inputs(i)._1)
-            poke(c.io.in.bits.b, inputs(i)._2)
-            poke(c.io.in.valid, en)
-
-            step(1)
+            poke(c.io.thresh, thresh)
+            poke(c.io.data, inputs(i))
+            poke(c.io.en, en)
 
             expect(c.io.out, outputs(i) & en)
         }
