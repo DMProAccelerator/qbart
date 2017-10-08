034063a096f6b8a49c0bd73177ddb1bda70a4647
Fredrik Ås <fredaas86@gmail.com>
2017-10-08 15:24:18 +0200

    Remove flush port. Add comments.

--- threshold/prototype/chisel/ThresholdingUnit.scala
+++ threshold/prototype/chisel/ThresholdingUnit.scala
@@ -4,18 +4,17 @@ import Chisel._
 
 
 /**
- * Compares two values and accumulates their corresponding Hamming distance.
+ * Compares two values and accumulates their corresponding Hamming weight.
  *
  * Input  1: Matrix element.
  * Input  2: Threshold element.
- * Input  3: Reset bit indicating register flushing.
- * Output 1: Cumulative hamming distance.
+ * Output 1: Cumulative Hamming weight.
  * Output 2: Count-value indicating how many times this module has been applied
  *           on a single matrix element.
  *
  * The class provides a hard step function that compares a matrix element
- * M(i, j) to a threshold element T(k). If M(i, j) >= T(k), then the hamming
- * distance is incremented by one.
+ * M(i, j) to a threshold element T(k). If M(i, j) >= T(k), then the Hamming
+ * weight is incremented by one.
  */
 class ThresholdingCompareUnit extends Module {
 
@@ -23,21 +22,14 @@ class ThresholdingCompareUnit extends Module {
     val io = new Bundle {
         val matrix_element = UInt(INPUT, 32)
         val threshold_element = UInt(INPUT, 32)
-        val reset = UInt(INPUT, 1)
         val hamming_weight = UInt(OUTPUT, 32)
         val count = UInt(OUTPUT, 32)
     }
 
     // Allocate registers and step function.
-    val count_r = Reg(init = UInt(0, 32))
     val accumulator_r = Reg(init = UInt(0, 32))
+    val count_r = Reg(init = UInt(0, 32))
     val step = UInt(width = 1)
-
-    // Flush all registers when reset bit is set.
-    when (reset) {
-        accumulator_r := UInt(0)
-        count_r := UInt(0)
-    }
 
     // Apply step function.
     step := UInt(0)
@@ -45,26 +37,30 @@ class ThresholdingCompareUnit extends Module {
         step := UInt(1)
     }
 
-    // Accumulate results.
+    // Accumulate and output results.
     accumulator_r := accumulator_r + step
     count_r := count_r + UInt(1)
-
-    // Output results.
-    io.count := count_r
     io.hamming_weight := accumulator_r
+    io.count := count_r
 
 }
 
+
+/**
+ * Performs tests on the ThresholdingCompareUnit accumulator. Ensures valid
+ * cumulative Hamming weight for randomly generated matrix and threshold
+ * elements.
+ */
 class ThresholdingUnitTests(c: ThresholdingCompareUnit) extends Tester(c) {
 
     var matrix_element_t = 0
     var threshold_element_t = 0
     var hamming_weight_t = 0
-    var reset_t = 0
     var count_t = 0
+    var threshold_length = 4
 
-    for (_ <- 0 to 3) {
-        // Generate random test values.
+    for (_ <- 1 to threshold_length) {
+        // Generate random test-values.
         matrix_element_t = rnd.nextInt(Int.MaxValue)
         threshold_element_t = rnd.nextInt(Int.MaxValue)
 
@@ -81,7 +77,6 @@ class ThresholdingUnitTests(c: ThresholdingCompareUnit) extends Tester(c) {
         // Set input and compare results.
         poke(c.io.matrix_element, matrix_element_t)
         poke(c.io.threshold_element, threshold_element_t)
-        poke(c.io.reset, reset_t)
         step(1)
         expect(c.io.hamming_weight, hamming_weight_t)
         expect(c.io.count, count_t)
