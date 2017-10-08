b69e57bb8228477506310733fe29bee954fc9b9d
Fredrik Ås <fredaas86@gmail.com>
2017-10-08 15:26:40 +0200

    Remove Adder dummy. Remove CompareUnit. Modify test suite.

--- threshold/prototype/chisel/init.scala
+++ threshold/prototype/chisel/init.scala
@@ -6,17 +6,9 @@ object Prototypes {
     def main(args: Array[String]): Unit = {
         val tutArgs = args.slice(1, args.length)
         args(0) match {
-            case "Adder" =>
-                chiselMainTest(tutArgs, () => Module(new Adder())) {
-                    c => new AdderTests(c)
-                }
             case "ThresholdingCompareUnit" =>
                 chiselMainTest(tutArgs, () => Module(new ThresholdingCompareUnit())) {
                     c => new ThresholdingUnitTests(c)
-                }
-            case "CompareUnit" =>
-                chiselMainTest(tutArgs, () => Module(new CompareUnit())) {
-                    c => new CompareUnitTests(c)
                 }
         }
     }
