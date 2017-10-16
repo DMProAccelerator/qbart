ece77a9785e1ccf7cc91586af5842cbcde1638b5
aliccce <aliceg@outlook.com>
2017-10-16 17:11:20 +0200

    Restructuring folders

--- prototypes/thresholding/chisel/thresholding-several-compare-units/init.scala
+++ prototypes/thresholding/chisel/thresholding-several-compare-units/init.scala
@@ -2,6 +2,18 @@ package Prototypes
 
 import Chisel._
 
+/** Loads and executes module tests.
+ *
+ * A module is added to the loader the following way:
+ *
+ * case "<module-name>" =>
+ *     chiselMainTest(params, () => Module(new <module-name>())) {
+ *         c = new <module-test-name>(c) }
+ *
+ * All modules contained in the build directory are compiled and lodaded during
+ * testing.
+ */
+
 object Prototypes {
     def main(args: Array[String]): Unit = {
         val tutArgs = args.slice(1, args.length)
--- threshold/prototype/chisel/build.sbt
+++
@@ -1,9 +0,0 @@
-organization := "edu.berkeley.cs"
-
-version := "2.3-SNAPSHOT"
-
-name := "prototypes"
-
-scalaVersion := "2.11.7"
-
-scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-language:reflectiveCalls")
