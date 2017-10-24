74f37b349a64f75dbe4b5381ee3f0a955ab9c72f
Fredrik Ås <fredaas86@gmail.com>
2017-10-24 12:12:02 +0200

    Add thresholding unit to FPGA entry point.

--- rosetta/src/main/cpp/app/main.cpp
+++ rosetta/src/main/cpp/app/main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
-using namespace std;
+#include <cstdlib>
+#include <ctime>
 
 #include "TestRegOps.hpp"
+#include "ThresholdingUnit.hpp"
+
 #include "platform.h"
+
+#define SIZE_MATRIX 10
+#define SIZE_THRESH 4
+
+using namespace std;
 
 bool Run_TestRegOps(WrapperRegDriver * platform) {
   TestRegOps t(platform);
@@ -20,11 +28,63 @@ bool Run_TestRegOps(WrapperRegDriver * platform) {
   return (a+b) == t.get_sum();
 }
 
+void Run_ThresholdingUnit(WrapperRegDriver * platform) {
+  ThresholdingUnit t(platform);
+
+  cout << "Signature: " << hex << t.get_signature() << dec << endl;
+
+  srand(time(NULL));
+
+  uint32_t matrix[SIZE_MATRIX];
+  uint32_t threshold[SIZE_THRESH];
+  uint32_t result[SIZE_MATRIX];
+
+  for (int i = 0; i < SIZE_MATRIX; i++) {
+    matrix[i] = rand() % 256;
+  }
+  for (int i = 0; i < SIZE_THRESH; i++) {
+    threshold[i] = rand() % 256;
+  }
+
+  // Calculate results.
+  for (int i = 0; i < SIZE_MATRIX; i++) {
+    uint32_t x = matrix[i];
+    uint32_t count = 0;
+    for (int j = 0; j < SIZE_THRESH; j++) {
+      count += (x >= threshold[j]);
+    }
+    result[i] = count;
+  }
+
+  t.set_op_0(threshold[0]);
+  t.set_op_1(threshold[1]);
+  t.set_op_2(threshold[2]);
+  t.set_op_3(threshold[3]);
+
+  t.set_size(SIZE_THRESH);
+
+  for (int i = 0; i < SIZE_MATRIX; i++) {
+    t.set_matrix_bits(matrix[i]);
+    t.set_matrix_valid(1);
+    t.set_start(1);
+
+    while (t.get_count_valid() == 0);
+
+    cout << "Result: " << t.get_count_bits() << " Expected: " << result[i]
+      << endl;
+
+    t.set_start(0);
+    t.set_matrix_valid(0);
+  }
+
+  cout << "CC: " << t.get_cc() << endl;
+}
+
 int main()
 {
   WrapperRegDriver * platform = initPlatform();
 
-  Run_TestRegOps(platform);
+  Run_ThresholdingUnit(platform);
 
   deinitPlatform(platform);
 
