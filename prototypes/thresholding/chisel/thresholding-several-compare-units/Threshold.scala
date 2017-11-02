ff8c87da124c6911f98df754e9ad7dcdc6c236eb
aliccce <aliceg@outlook.com>
2017-11-02 17:16:52 +0100

    Thresholding unit with state machine

--- prototypes/thresholding/chisel/thresholding-several-compare-units/Threshold.scala
+++ prototypes/thresholding/chisel/thresholding-several-compare-units/Threshold.scala
@@ -4,68 +4,108 @@ import Chisel._
 
 class ThresholdThreshInput(val n: Int) extends Bundle {
     val in = Vec( n, Bits(INPUT, 8) )
-    val en = Vec( n, Bits(INPUT, 1) )
+    val en = Vec( n, Bool(INPUT) )
 
     override def cloneType: this.type = new ThresholdThreshInput(4).asInstanceOf[this.type]
 }
 
-class ThresholdDataInput extends Bundle {
-    val data = Bits(INPUT, 8)
+class ThresholdElementInput extends Bundle {
+    val element = Bits(INPUT, 8)
 }
 
 class Threshold(val n: Int) extends Module {
     val io = new Bundle{
-        val reset = Bool(INPUT)
+        val start = Bool(INPUT)
         val thresh = Decoupled( new ThresholdThreshInput(n) ).flip()
-        val in = Decoupled( new ThresholdDataInput() ).flip()
+        val in = Decoupled( new ThresholdElementInput() ).flip()
         val out = Valid(Bits(width = 8))
         val cycle = UInt(OUTPUT)
+
+        val state_out = UInt(OUTPUT)
+        val element_out = UInt(OUTPUT)
     }
 
-    io.out.bits := UInt(255)
-    io.out.valid := Bool(false)
+    val s_idle :: s_running :: s_finished :: Nil = Enum(UInt(), 3)
+    val state = Reg( init = UInt(s_idle) )
 
-    val data = Reg( UInt(width = 8) )
+    val check_valid = Reg( init = Bool(false) )
+    val element = Reg( UInt(width = 8) )
+    val p_element = Reg(init = Bool(true))
+    val p_thresh = Reg(init = Bool(true))
     val cycle = Reg( init=UInt(0, width = 8) )
-    cycle := cycle + UInt(1)
-    io.cycle := cycle
 
-
-    when ( io.reset ) {
-        // Cycle will be 0 in the next cycle.
-        cycle := UInt(0)
-        io.out.bits := UInt(1)
-    }
-
-    val p_data = Bool(false)
-    val p_thresh = Bool(false)
+    // Create n comparing units
     val cmp_out = Wire( Vec( n, Bool() ))
-    io.in.ready := !p_data
-
     val cmp_units = for (i <- 0 until n ) yield
     {
         val cmp = Module( new CompareUnit() )
-        cmp.io.data := data
+        cmp.io.data := element
         cmp.io.thresh := io.thresh.bits.in(i)
         cmp.io.en := io.thresh.bits.en(i)
         cmp_out(i) := cmp.io.out
     }
 
+    val cmp_output = RegNext( cmp_out )
 
-    when( io.in.valid && !p_data ) {
-        // There is valid data on the line, and unit is ready to receive.
-        data := io.in.bits.data
-        p_data := Bool(true)
-    }
+    // Default register values
+    check_valid := Bool(false)
+    p_thresh := Bool(false)
+    cycle := cycle
 
-    when( io.thresh.valid && !p_thresh ) {
-        // There are valid thresholds on the line, and unit is ready to receive.
-        io.out.valid := !cmp_out(n - 1)
-        io.out.bits := (cycle) * UInt(n) + PopCount( cmp_out )
-        p_thresh := Bool(true)
+    // Default OUTPUT values
+    io.cycle := cycle
+    io.in.ready := !p_element
+    io.thresh.ready := !p_thresh
+    io.out.bits := UInt(255)
+    io.out.valid := Bool(false)
+
+    // DEBUG VALUES
+    io.state_out := state
+    io.element_out := element
+
+    switch(state) {
+        is(s_idle){
+            p_thresh := Bool(true)
+
+            when( io.start ) {
+                p_thresh := Bool(false)
+                p_element := Bool(false)
+                cycle := UInt(0)
+                state := s_running
+            }
+        }
+
+        is(s_running) {
+
+            when( check_valid ) {
+                when( cmp_out(n - 1) === UInt(0) ) {
+                    // When the last comparing unit value is zero, the threshold output is found.
+                    state := s_finished
+                }
+            }
+
+            when( io.in.valid && !p_element ) {
+                // There is valid data on the line, and unit is ready to receive.
+                element := io.in.bits.element
+                p_element := Bool(true)
+            }
+
+            when( io.thresh.valid && !p_thresh ) {
+                // There are valid thresholds on the line, and unit is ready to receive.
+                cycle := cycle + UInt(1)
+                p_thresh := Bool(true)
+                check_valid := Bool(true)
+            }
+        }
+
+        is(s_finished) {
+            p_thresh := Bool(true)
+            io.out.valid := Bool(true)
+            io.out.bits := (cycle - UInt(1)) * UInt(n) + PopCount( cmp_output )
+            state := s_idle
+        }
     }
 }
-
 
 
 class ThresholdTests(c: Threshold) extends Tester(c) {
@@ -74,83 +114,146 @@ class ThresholdTests(c: Threshold) extends Tester(c) {
     val outputs = List(  3,  0,  0,  1,  1,   2,   3,   3,   3,   2,  1,  0,   3 )
 
     for ( i <- 0 until inputs.size ) {
-        poke( c.io.reset, 1 )
+        expect( c.io.state_out, 0 )
 
-        // Give the unit the data
-        poke( c.io.in.bits.data, inputs(i) )
+        poke( c.io.start, 1 )
+
+
+        step( 10 )
+        expect( c.io.state_out, 1 )
+
+        poke( c.io.start, 0 )
+
+        // Give the matrix element
+        poke( c.io.in.bits.element, inputs(i) )
         poke( c.io.in.valid, 1 )
 
-        // Give it the thresholds
-        poke(c.io.thresh.bits.in(0), 50)
+        // Give the thresholds
+        poke(c.io.thresh.bits.in(0), thresholds(0) )
         poke(c.io.thresh.bits.en(0), 1)
 
-        poke(c.io.thresh.bits.in(1), 100)
+        poke(c.io.thresh.bits.in(1), thresholds(1) )
         poke(c.io.thresh.bits.en(1), 1)
 
-        poke(c.io.thresh.bits.in(2), 150)
+        poke(c.io.thresh.bits.in(2), thresholds(2) )
         poke(c.io.thresh.bits.en(2), 1)
 
         poke(c.io.thresh.bits.en(3), 0)
 
         poke(c.io.thresh.valid, 1)
 
-        step(1)
-        poke( c.io.reset, 0 )
+        expect( c.io.out.valid, 0 )
+        expect( c.io.cycle, 0 )
 
+        step( 1 )
+
+
+        poke(c.io.in.valid, 0)
+        poke(c.io.thresh.valid, 0)
+
+        // Check outputs
+
+        expect( c.io.state_out, 1 )
+        expect( c.io.out.valid, 0 )
+        expect( c.io.cycle, 1 )
+
+        step( 1 )
+
+        expect( c.io.element_out, inputs(i) )
+
+
+        expect( c.io.state_out, 2 )
         expect( c.io.out.valid, 1 )
+        expect( c.io.cycle, 1 )
+
         expect( c.io.out.bits, outputs(i) )
+
+        step( 10 )
     }
-
-
 }
 
 
 class ThresholdWithCyclesTests(c: Threshold) extends Tester(c){
     val thresholds = List( 50, 100, 150 )
     val inputs  =   List( 16, 28, 40, 52, 89, 128, 175, 228, 255, 140, 80, 20, 180 )
-    val one_cycle = List(  1,   1,  1, 1,  1,   0,   0,   0,   0,   0,  1,  1,   0 )
+    val num_cycles = List(  1,   1,  1, 1,  1,   2,   2,   2,   2,   2,  1,  1,   2 )
     val outputs =   List(  0,  0,  0,  1,  1,   2,   3,   3,   3,   2,  1,  0,   3 )
 
     for ( i <- 0 until inputs.size ) {
-        // Expect that the unit is ready to receive
-        expect( io.in.ready, 1 )
+        // Start the unit
+        poke( c.io.start, 1 )
 
-        // Give the data
-        poke( c.io.reset, 1 )
-        poke( c.io.in.bits.data, inputs(i) )
+        // while ( peekAt(c.io.in.ready) == 0 && peekAt(c.io.thresh.ready) == 0 ) {
+        step(10)
+        // }
+
+        // We should expect this to be the first cycle, with invalid output.
+        // And that the unit is now ready to receive.
+        expect( c.io.cycle, 0 )
+        expect( c.io.out.valid, 0 )
+        expect( c.io.in.ready, 1 )
+        expect( c.io.thresh.ready, 1 )
+
+        poke( c.io.start, 0 )
+
+        // Give the matrix element
+        poke( c.io.in.bits.element, inputs(i) )
         poke( c.io.in.valid, 1 )
 
-        // Give two thresholds
-        poke( c.io.thresh.bits.in(0), thresholds(0) )
-        poke( c.io.thresh.bits.en(0), 1 )
+        // Give the first two thresholds
+        poke(c.io.thresh.bits.in(0), thresholds(0) )
+        poke(c.io.thresh.bits.en(0), 1)
 
-        poke( c.io.thresh.bits.in(1), thresholds(1) )
-        poke( c.io.thresh.bits.en(1), 1 )
-        poke( c.io.thresh.valid, 1 )
+        poke(c.io.thresh.bits.in(1), thresholds(1) )
+        poke(c.io.thresh.bits.en(1), 1)
+        poke(c.io.thresh.valid, 1 )
 
-        // Next cycle
+        // while ( c.io.thresh.ready == 0 && c.io.out.valid == 0 ) {
         step(1)
-        poke( c.io.reset, 0 )
-        poke( c.io.in.valid, 0 )
+        // }
+        expect( c.io.thresh.ready, 0 )
+        expect( c.io.out.valid, 0 )
 
-        if ( one_cycle(i) == 1 ) {
-            expect( c.io.out.valid, 1 )
-            expect( c.io.out.bits, outputs(i) )
-        }
-        else {
+        poke( c.io.thresh.valid, 0 )
+
+        // Data is fed to register, so no need to care about this.
+        poke(c.io.in.bits.element, 0)
+        poke(c.io.in.valid, 0)
+
+        step(1)
+
+
+        if ( num_cycles(i) == 2 ) {
+
+            // This should make no difference
+            step(10)
+
             expect( c.io.out.valid, 0 )
-            expect( c.io.in.ready, 0 )
+            expect( c.io.thresh.ready, 1 )
 
-            // Give last threshold
+            // We start our second cycle, one wasn't enough.
             poke( c.io.thresh.bits.in(0), thresholds(2) )
             poke( c.io.thresh.bits.en(0), 1 )
 
             poke( c.io.thresh.bits.en(1), 0 )
 
-            // Next cycle
+            poke( c.io.thresh.valid, 1 )
+
+
             step(1)
-            expect( c.io.out.valid, 1 )
-            expect( c.io.out.bits, outputs(i) )
+
+            expect( c.io.thresh.ready, 0 )
+            expect( c.io.out.valid, 0 )
+
+            poke( c.io.thresh.valid, 0 )
+
+            step(1)
         }
+
+        expect( c.io.out.valid, 1 )
+        expect( c.io.out.bits, outputs(i) )
+        expect( c.io.cycle, num_cycles(i) )
+
+        step(1)
     }
 }
